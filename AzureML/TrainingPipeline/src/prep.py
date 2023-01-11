a3a50db36559b560b11ea01cb0a28a6338416900
David Hurley <83986810+dhurley-suncor@users.noreply.github.com>
2023-01-11 15:19:38 -0700

    Update prep.py

--- AzureML/TrainingPipeline/src/prep.py
+++ AzureML/TrainingPipeline/src/prep.py
@@ -1,4 +1,4 @@
-""" Step 1 - prepare dataset for training """
+""" Step 1 - prepare dataset for training trigger workflow """
 
 ####################################################################################
 # IMPORTS
