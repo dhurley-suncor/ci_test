2220f2ddd263b74f7062ae5c42ed6726fb860b57
david-hurley <hurleyldave@gmail.com>
2023-01-12 11:08:56 -0700

    file to build realtime api

---
+++ AzureML/RealTimeInference/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request
+import numpy as np
+import pickle
+
+app = Flask(__name__)
+
+linear_model = pickle.load(open('model.pkl', 'rb'))
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+  payload = request.get_json(force=True)
+
+  data = payload['data']
+
+  value_to_predict = np.array([data]).reshape(-1, 1)
+
+  prediction = linear_model.predict(value_to_predict)
+  prediction = str(prediction[0][0])
+
+  return prediction
